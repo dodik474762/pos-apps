bd6d4c1419fac53b95a35ebef67059afcdf36aea
dodik <dodikra8@gmail.com>
2025-10-12 20:18:47 +0700

    updatre

--- resources/views/web/template/leftmenu.blade.php
+++ resources/views/web/template/leftmenu.blade.php
@@ -28,23 +28,23 @@
     <!-- LOGO -->
     <div class="navbar-brand-box">
         <!-- Dark Logo-->
-        <a href="{{ url('/dashboard') }}" class="logo logo-dark" style="background-color: white;width: 100%;border-radius: 3px;padding: 8px;box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);">
+        <a href="{{ url('/dashboard') }}" class="logo logo-dark" style="">
             <span class="logo-sm">
                 <img src="{{ asset('main_template/assets/images/logo-sm.png') }}" alt="" height="22">
             </span>
-            <span class="logo-lg">
-                <img src="{{ asset('assets/images/logo_dpi.png') }}" alt="" height="50"
-                    width="150">
+            <span class="logo-lg" style="margin-right:120px;">
+                <img src="{{ asset('assets/images/logo-main-app.png') }}" alt="" height="50"
+                    width="70">
             </span>
         </a>
         <!-- Light Logo-->
         <a href="{{ url('/dashboard') }}" class="logo logo-light"
-            style="background-color: white;width: 100%;border-radius: 3px;padding: 8px;box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);">
+            style="">
             <span class="logo-sm">
                 <img src="{{ asset('main_template/assets/images/logo-sm.png') }}" alt="" height="22">
             </span>
-            <span class="logo-lg">
-                <img src="{{ asset('assets/images/logo_dpi.png') }}" alt="" height="50" width="150">
+            <span class="logo-lg" style="margin-right:120px;">
+                <img src="{{ asset('assets/images/logo-app-v2.png') }}" alt="" height="70" width="70">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
