1c8fd0b9180a11e89654fb3bb1d3fe0b45b989d9
EDP <solutionsdapps@gmail.com>
2026-06-04 20:20:10 +0700

    update

--- public/assets/js/controllers/master/product.js
+++ public/assets/js/controllers/master/product.js
@@ -106,7 +106,14 @@ let Product = {
             createdRow: function (row, data, dataIndex) {
                 // console.log('row', $(row));
             },
-            buttons: ["copy", "excel", "pdf", "colvis"],
+            dom: "Bftrip",
+            buttons: [
+                {
+                    extend: "excel",
+                    filename: "Product",
+                    action: newexportaction,
+                },
+            ],
             columns: [
                 {
                     data: "id",
@@ -154,9 +161,8 @@ let Product = {
                         if (updateAction == 1) {
                             html += `<a href='${url.base_url(
                                 Product.module(),
-                            )}ubah?id=${data}' data_id="${
-                                row.id
-                            }" class="btn btn-success editable-submit btn-sm waves-effect waves-light"><i class="bx bx-edit"></i></a>&nbsp;`;
+                            )}ubah?id=${data}' data_id="${row.id
+                                }" class="btn btn-success editable-submit btn-sm waves-effect waves-light"><i class="bx bx-edit"></i></a>&nbsp;`;
                         }
                         if (deleteAction == 1) {
                             html += `<button type="button" data_id="${row.id}" onclick="Product.delete(this, event)" class="btn btn-danger editable-cancel btn-sm waves-effect waves-light"><i class="bx bx-trash-alt"></i></button>`;
@@ -965,6 +971,92 @@ let Product = {
         });
     },
 };
+
+
+// untuk export all data
+function newexportaction(e, dt, button, config) {
+    var self = this;
+    var oldStart = dt.settings()[0]._iDisplayStart;
+    dt.one("preXhr", function (e, s, data) {
+        // Just this once, load all data from the server...
+        data.start = 0;
+        data.length = 2147483647;
+        dt.one("preDraw", function (e, settings) {
+            // Call the original action function
+            if (button[0].className.indexOf("buttons-copy") >= 0) {
+                $.fn.dataTable.ext.buttons.copyHtml5.action.call(
+                    self,
+                    e,
+                    dt,
+                    button,
+                    config,
+                );
+            } else if (button[0].className.indexOf("buttons-excel") >= 0) {
+                $.fn.dataTable.ext.buttons.excelHtml5.available(dt, config)
+                    ? $.fn.dataTable.ext.buttons.excelHtml5.action.call(
+                        self,
+                        e,
+                        dt,
+                        button,
+                        config,
+                    )
+                    : $.fn.dataTable.ext.buttons.excelFlash.action.call(
+                        self,
+                        e,
+                        dt,
+                        button,
+                        config,
+                    );
+            } else if (button[0].className.indexOf("buttons-csv") >= 0) {
+                $.fn.dataTable.ext.buttons.csvHtml5.available(dt, config)
+                    ? $.fn.dataTable.ext.buttons.csvHtml5.action.call(
+                        self,
+                        e,
+                        dt,
+                        button,
+                        config,
+                    )
+                    : $.fn.dataTable.ext.buttons.csvFlash.action.call(
+                        self,
+                        e,
+                        dt,
+                        button,
+                        config,
+                    );
+            } else if (button[0].className.indexOf("buttons-pdf") >= 0) {
+                $.fn.dataTable.ext.buttons.pdfHtml5.available(dt, config)
+                    ? $.fn.dataTable.ext.buttons.pdfHtml5.action.call(
+                        self,
+                        e,
+                        dt,
+                        button,
+                        config,
+                    )
+                    : $.fn.dataTable.ext.buttons.pdfFlash.action.call(
+                        self,
+                        e,
+                        dt,
+                        button,
+                        config,
+                    );
+            } else if (button[0].className.indexOf("buttons-print") >= 0) {
+                $.fn.dataTable.ext.buttons.print.action(e, dt, button, config);
+            }
+            dt.one("preXhr", function (e, s, data) {
+                // DataTables thinks the first item displayed is index 0, but we're not drawing that.
+                // Set the property to what it was before exporting.
+                settings._iDisplayStart = oldStart;
+                data.start = oldStart;
+            });
+            // Reload the grid with the original page. Otherwise, API functions like table.cell(this) don't work properly.
+            setTimeout(dt.ajax.reload, 0);
+            // Prevent rendering of the full data to the DOM
+            return false;
+        });
+    });
+    // Requery the server with the new one-time export settings
+    dt.ajax.reload();
+}
 
 $(function () {
     Product.setSelect2();
