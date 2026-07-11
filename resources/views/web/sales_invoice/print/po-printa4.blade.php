c6f46f4841e1c44459f4182f8f48e25bba86f157
EDP <solutionsdapps@gmail.com>
2026-07-11 15:51:08 +0700

    update

--- resources/views/web/sales_invoice/print/po-printa4.blade.php
+++ resources/views/web/sales_invoice/print/po-printa4.blade.php
@@ -12,7 +12,7 @@
         @page {
             size: 210mm 148.5mm;
             /*margin: 0;*/
-            margin: 5mm 2mm 0mm 2mm;
+            margin: 10mm 2mm 0mm 0mm;
         }
 
         * {
