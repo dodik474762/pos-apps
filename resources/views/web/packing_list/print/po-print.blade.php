dee0cd4416fe649232216dda84a47bab4dba586d
EDP <solutionsdapps@gmail.com>
2026-07-11 10:00:38 +0700

    [pdate

--- resources/views/web/packing_list/print/po-print.blade.php
+++ resources/views/web/packing_list/print/po-print.blade.php
@@ -348,7 +348,7 @@
             $size = 9;
             $text = "Halaman {PAGE_NUM} dari {PAGE_COUNT}";
 
-            $y = $pdf->get_height() - 40;
+            $y = $pdf->get_height() - 60;
 
             // x = 0, width = full page width, align = center
             $pdf->page_text(
