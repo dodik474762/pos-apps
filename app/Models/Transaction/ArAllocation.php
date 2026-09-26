7adc1754e8b3608226e7ff37dc1ec69fcb4e54ff
EDP <solutionsdapps@gmail.com>
2026-09-26 13:18:37 +0700

    udpayte-model

---
+++ app/Models/Transaction/ArAllocation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Transaction;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ArAllocation extends Model
+{
+    protected $table = 'ar_allocations';
+}
---
+++ app/Models/Transaction/ArInvoice.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Transaction;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ArInvoice extends Model
+{
+    protected $table = 'ar_invoices';
+}
